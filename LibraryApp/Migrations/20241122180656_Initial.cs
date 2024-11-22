62aca938fa8dc6dc4db227a865dbd08e7d100612
Nikita Developer <nik0609tak@gmail.com>
2024-11-22 19:07:49 +0100

    Migrations were added

---
+++ LibraryApp/Migrations/20241122180656_Initial.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LibraryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: false),
+                    Genre = table.Column<string>(type: "text", nullable: false),
+                    Publisher = table.Column<string>(type: "text", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Genre", "Publisher", "Title", "Year" },
+                values: new object[,]
+                {
+                    { 1, "F. Scott Fitzgerald", "Fiction", "Scribner", "The Great Gatsby", 1925 },
+                    { 2, "George Orwell", "Dystopian", "Secker & Warburg", "1984", 1949 },
+                    { 3, "Harper Lee", "Fiction", "J.B. Lippincott & Co.", "To Kill a Mockingbird", 1960 },
+                    { 4, "Herman Melville", "Adventure", "Harper & Brothers", "Moby-Dick", 1851 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
